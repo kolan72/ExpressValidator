3b1a370e3b627c41796bfd7396bc118c1381fe17
kolan72 <kolan72@gmail.com>
2024-03-25 15:34:12 +0300

    Add test for `PropertyInfoParser.TryParse` method.

---
+++ tests/UtilitiesTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Reflection;
+
+namespace ExpressValidator.Tests
+{
+	internal class UtilitiesTests
+	{
+		[Test]
+		public void Should_PropertyInfoParser_TryParse_Work_Correctly()
+		{
+			var resParseS = PropertyInfoParser.TryParse<ObjWithTwoPublicProps, string>(s => s.S, out PropertyInfo propertyInfoS);
+			Assert.That(resParseS, Is.True);
+			Assert.That(propertyInfoS.Name, Is.EqualTo("S"));
+
+			var resParseI = PropertyInfoParser.TryParse<ObjWithTwoPublicProps, int>(s => s.I, out PropertyInfo propertyInfoI);
+			Assert.That(resParseI, Is.True);
+			Assert.That(propertyInfoI.Name, Is.EqualTo("I"));
+
+			var resParse = PropertyInfoParser.TryParse<ObjWithTwoPublicProps, ObjWithTwoPublicProps>(s => s, out PropertyInfo propertyInfo);
+			Assert.That(resParse, Is.False);
+		}
+	}
+}

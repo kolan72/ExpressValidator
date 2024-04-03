ec78af60ac0fedb76e7884bb87a48baa9044ecc7
kolan72 <kolan72@gmail.com>
2024-04-03 12:14:27 +0300

    Made `IExpressPropertyValidatorBase` internal.

--- src/PropertyValidators/IExpressPropertyValidatorBase.cs
+++ src/PropertyValidators/IExpressPropertyValidatorBase.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace ExpressValidator
 {
-	public interface IExpressPropertyValidatorBase<T>
+	internal interface IExpressPropertyValidatorBase<T>
 	{
 		void SetValidation(Action<IRuleBuilderOptions<T, T>> action);
 	}

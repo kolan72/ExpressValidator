07a64f31ecfde35b48e6047a4cb0b239cb475857
kolan72 <kolan72@gmail.com>
2024-06-28 15:23:47 +0300

    Slightly improve performance by using `PropertyInfo.GetGetMethod` to get a property from an object.

--- src/Utilities/ReflectionExtensions.cs
+++ src/Utilities/ReflectionExtensions.cs
@@ -13,7 +13,7 @@ namespace ExpressValidator
 
 		public static T GetTypedValue<TObj, T>(this MemberInfo memberInfo, TObj obj)
 		{
-			return memberInfo.MemberType == MemberTypes.Property ? (T)((PropertyInfo)memberInfo).GetValue(obj)
+			return memberInfo.MemberType == MemberTypes.Property ? (T)((PropertyInfo)memberInfo).GetGetMethod().Invoke(obj, null)
 																 : (T)((FieldInfo)memberInfo).GetValue(obj);
 		}
 	}

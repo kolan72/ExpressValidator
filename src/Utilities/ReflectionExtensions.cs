b9d12f92e361b5410870160b33e69337cb34355b
kolan72 <kolan72@gmail.com>
2024-06-28 15:28:35 +0300

    Remove redundant overload of the `PropertyInfo.GetTypedValue` extension method.

--- src/Utilities/ReflectionExtensions.cs
+++ src/Utilities/ReflectionExtensions.cs
@@ -6,11 +6,6 @@ namespace ExpressValidator
 {
 	internal static class ReflectionExtensions
 	{
-		public static T GetTypedValue<TObj, T>(this PropertyInfo propertyInfo, TObj obj)
-		{
-			return (T)propertyInfo.GetValue(obj);
-		}
-
 		public static T GetTypedValue<TObj, T>(this MemberInfo memberInfo, TObj obj)
 		{
 			return memberInfo.MemberType == MemberTypes.Property ? (T)((PropertyInfo)memberInfo).GetGetMethod().Invoke(obj, null)

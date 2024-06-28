6ed76b5aea8d2e1b0fbbaf3060949645d30e9a69
kolan72 <kolan72@gmail.com>
2024-06-28 15:15:07 +0300

    Add benchmark to get the fastest way to get `Func` for `PropertyInfo`.

--- src/Utilities/ReflectionExtensions.cs
+++ src/Utilities/ReflectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace ExpressValidator
 {
@@ -13,6 +15,34 @@ namespace ExpressValidator
 		{
 			return memberInfo.MemberType == MemberTypes.Property ? (T)((PropertyInfo)memberInfo).GetValue(obj)
 																 : (T)((FieldInfo)memberInfo).GetValue(obj);
+		}
+	}
+
+	public static class PropertyInfoToFuncConverter
+	{
+		public static Func<TObj, T> GetPropertyFuncByExpression<TObj, T>(MemberInfo memberInfo)
+		{
+			return BuildGetAccessor<TObj, T>(((PropertyInfo)memberInfo).GetGetMethod());
+		}
+
+		public static Func<TObj, T> GetPropertyFuncByReflection<TObj, T>(MemberInfo memberInfo)
+		{
+			return (obj) => (T)((PropertyInfo)memberInfo).GetValue(obj);
+		}
+
+		public static Func<TObj, T> GetPropertyFuncByMethodInfo<TObj, T>(MemberInfo memberInfo)
+		{
+			var m = ((PropertyInfo)memberInfo).GetGetMethod();
+			return (obj) => (T)m.Invoke(obj, null);
+		}
+
+		internal static Func<TObj, T> BuildGetAccessor<TObj, T>(MethodInfo method)
+		{
+			ParameterExpression instance = Expression.Parameter(typeof(TObj), "instance");
+
+			var body = Expression.Call(instance, method);
+
+			return Expression.Lambda<Func<TObj, T>>(body, instance).Compile();
 		}
 	}
 }

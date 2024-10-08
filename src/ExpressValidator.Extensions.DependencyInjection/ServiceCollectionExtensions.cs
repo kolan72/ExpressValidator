7606275be3f766779ddf1632545bbe01f7c06f4f
kolan72 <kolan72@gmail.com>
2024-10-08 12:38:10 +0300

    Add ExpressValidator.Extensions.DependencyInjection.csproj.

---
+++ src/ExpressValidator.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace ExpressValidator.Extensions.DependencyInjection
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddExpressValidator<T>(this IServiceCollection services, Action<ExpressValidatorBuilder<T>> configure, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
+		{
+			return AddExpressValidator(services, configure, new ExpressValidatorOptions() { OnFirstPropertyValidatorFailed = OnFirstPropertyValidatorFailed.Continue }, serviceLifetime);
+		}
+
+		public static IServiceCollection AddExpressValidator<T>(this IServiceCollection services, Action<ExpressValidatorBuilder<T>> configure, ExpressValidatorOptions expressValidatorOptions, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
+		{
+			IExpressValidator<T> func(IServiceProvider sp)
+			{
+				var s = sp.GetRequiredService<IOptions<ExpressValidatorOptions>>();
+				var eb = new ExpressValidatorBuilder<T>(s.Value.OnFirstPropertyValidatorFailed);
+				configure(eb);
+				return eb.Build();
+			}
+			services.AddOptions<ExpressValidatorOptions>()
+					.Configure(options =>
+										options.OnFirstPropertyValidatorFailed = expressValidatorOptions.OnFirstPropertyValidatorFailed);
+			services.Add(new ServiceDescriptor(typeof(IExpressValidator<T>), func, serviceLifetime));
+			return services;
+		}
+	}
+}

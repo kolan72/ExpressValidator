78e3968059b156266fa01c3af9bcbac9664aaa6a
kolan72 <kolan72@gmail.com>
2024-12-17 12:38:55 +0300

    Add the `OptionsMonitorContext<TOptions>` class that encapsulates options changes handled by `IOptionsMonitor<TOptions>`.

---
+++ src/ExpressValidator.Extensions.DependencyInjection/OptionsMonitorContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+
+namespace ExpressValidator.Extensions.DependencyInjection
+{
+	internal class OptionsMonitorContext<TOptions> : IOptionsMonitorContext<TOptions>
+	{
+		private TOptions _options;
+
+		private DateTimeOffset _lastUpdated;
+
+		private readonly object _sync = new object();
+
+		public OptionsMonitorContext(IOptionsMonitor<TOptions> optionsMonitor)
+		{
+			_options = optionsMonitor.CurrentValue;
+			_lastUpdated = DateTimeOffset.UtcNow;
+
+			optionsMonitor.OnChange((newValue, _) =>
+			{
+				lock (_sync)
+				{
+					_options = newValue;
+					_lastUpdated = DateTimeOffset.UtcNow;
+				}
+			});
+		}
+
+		public TOptions Options
+		{
+			get
+			{
+				lock (_sync)
+				{
+					return _options;
+				}
+			}
+		}
+
+		public DateTimeOffset LastUpdated
+		{
+			get
+			{
+				lock (_sync)
+				{
+					return _lastUpdated;
+				}
+			}
+		}
+	}
+}

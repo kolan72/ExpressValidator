c89a78a7b585bde7ab45638b0bc2f3a4fd1632e0
kolan72 <kolan72@gmail.com>
2025-01-01 20:28:24 +0300

    Add doc comments to  the `IExpressValidatorBuilder<TObj>` interface.

--- src/ExpressValidator/ValidatorBuilders/IExpressValidatorBuilder.cs
+++ src/ExpressValidator/ValidatorBuilders/IExpressValidatorBuilder.cs
@@ -1,7 +1,15 @@
 ﻿namespace ExpressValidator
 {
+	/// <summary>
+	/// Represents the interface for a builder to create an instance of an object that implements <see cref="IExpressValidator{TOb}"/>.
+	/// </summary>
+	/// <typeparam name="TObj">Object to validate.</typeparam>
 	public interface IExpressValidatorBuilder<TObj>
 	{
+		/// <summary>
+		/// Creates an instance of an object that implements the <see cref="IExpressValidator{TOb}"/>.
+		/// </summary>
+		/// <returns><see cref="IExpressValidator{TOb}"/>.</returns>
 		IExpressValidator<TObj> Build();
 	}
 }

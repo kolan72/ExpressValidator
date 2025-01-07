306a6e7204eccbc71ef2b6d206073214d3ee9604
kolan72 <kolan72@gmail.com>
2025-01-07 15:52:57 +0300

    Add doc comment to `ExpressValidatorOptions.OnFirstPropertyValidatorFailed`.

--- src/ExpressValidator.Extensions.DependencyInjection/ExpressValidatorOptions.cs
+++ src/ExpressValidator.Extensions.DependencyInjection/ExpressValidatorOptions.cs
@@ -5,6 +5,9 @@
 	/// </summary>
 	public class ExpressValidatorOptions
 	{
+		/// <summary>
+		/// Specifies how an ExpressValidatorBuilder will validate an object if the current validator fails.
+		/// </summary>
 		public OnFirstPropertyValidatorFailed OnFirstPropertyValidatorFailed { get; set; }
 	}
 }

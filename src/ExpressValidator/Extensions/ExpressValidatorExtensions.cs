18ebc09384f78b311efe1a1f10430bb34b20a016
kolan72 <kolan72@gmail.com>
2025-01-31 13:38:17 +0300

    Introduce  `IExpressValidatorBuilder<TObj>.BuildAndValidate(TObj obj)` extension method.

---
+++ src/ExpressValidator/Extensions/ExpressValidatorExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Results;
+
+namespace ExpressValidator.Extensions
+{
+	public static class ExpressValidatorExtensions
+	{
+		/// <summary>
+		/// Builds an <see cref="IExpressValidatorBuilder{TObj}"/> to create a validator and validate an <paramref name="obj"/> object using the created validator.
+		/// </summary>
+		/// <typeparam name="TObj">A type of object to validate.</typeparam>
+		/// <param name="validatorBuilder"><see cref="IExpressValidatorBuilder{TObj}"/></param>
+		/// <param name="obj">An object instance to validate.</param>
+		/// <returns><see cref="ValidationResult"/></returns>
+		public static ValidationResult BuildAndValidate<TObj>(this IExpressValidatorBuilder<TObj> validatorBuilder, TObj obj)
+		{
+			return validatorBuilder.Build().Validate(obj);
+		}
+	}
+}
---
+++ tests/ExpressValidator.Tests/ExpressValidatorExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using FluentValidation;
+using ExpressValidator.Extensions;
+
+namespace ExpressValidator.Tests
+{
+	internal class ExpressValidatorExtensionsTests
+	{
+		[Test]
+		[TestCase(true)]
+		[TestCase(false)]
+		public void Should_BuildAndValidate_Work(bool isValid)
+		{
+			int i = isValid ? 1 : -1;
+			var objToValidate = new ObjWithTwoPublicProps() { I = i };
+
+			var result = new ExpressValidatorBuilder<ObjWithTwoPublicProps>()
+			   .AddProperty((o) => o.I)
+			   .WithValidation((opt) => opt.GreaterThan(0))
+			   .BuildAndValidate(objToValidate);
+
+			Assert.That(result.IsValid, Is.EqualTo(isValid));
+		}
+	}
+}

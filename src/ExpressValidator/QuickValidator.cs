8d234458398beae04f3c0b7dd06980bbeeaf0427
kolan72 <kolan72@gmail.com>
2025-04-23 18:19:07 +0300

    Introduced the `QuickValidator` class and its `Validate<T>(T, Action<IRuleBuilderOptions<T, T>>, string)` method.

---
+++ src/ExpressValidator/QuickValidator.cs
@@ -0,0 +1,33 @@
+﻿using ExpressValidator.Extensions;
+using FluentValidation;
+using FluentValidation.Results;
+using System;
+
+namespace ExpressValidator
+{
+	/// <summary>
+	/// Provides methods for quick validation.
+	/// </summary>
+	public static class QuickValidator
+	{
+		/// <summary>
+		///  Validates the given object instance using <paramref name="action"/>.
+		/// </summary>
+		/// <typeparam name="T">The type of the object to validate.</typeparam>
+		/// <param name="obj">The object to validate.</param>
+		/// <param name="action">Action to add validators.</param>
+		/// <param name="propName">The name of the property if the validation fails.
+		/// If <see langword="null"/>, "input value" will be used.</param>
+		/// <returns></returns>
+		public static ValidationResult Validate<T>(T obj, Action<IRuleBuilderOptions<T, T>> action, string propName = null)
+		{
+			var eb = new ExpressValidatorBuilder<Unit>();
+			return eb.AddFunc((_) => obj,
+								GetPropName(propName))
+					.WithValidation(action)
+					.BuildAndValidate(Unit.Default);
+		}
+
+		private static string GetPropName(string propName = null) => propName ?? "input value";
+	}
+}
---
+++ tests/ExpressValidator.Tests/QuickValidatorTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using NUnit.Framework;
+
+namespace ExpressValidator.Tests
+{
+	internal class QuickValidatorTests
+	{
+		[Test]
+		public void Should_Fail_Validation_When_NotValid()
+		{
+			const int valueToTest = 5;
+			var result = QuickValidator.Validate(valueToTest,
+									(opt) => opt.GreaterThan(10)
+												.GreaterThan(15));
+			Assert.That(result.IsValid, Is.False);
+			Assert.That(result.Errors.Count, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void Should_Pass_Validation_When_Valid()
+		{
+			const int valueToTest = 25;
+			var result = QuickValidator.Validate(valueToTest,
+									(opt) => opt.GreaterThan(10)
+												.InclusiveBetween(15, 25));
+			Assert.That(result.IsValid, Is.True);
+		}
+	}
+}

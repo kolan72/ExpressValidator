a834eac11fc92e65e58764425cbb4caa763ee4e7
kolan72 <kolan72@gmail.com>
2026-02-05 13:15:49 +0300

    Introduce abstract `ValidatorProfile<T>` class.

---
+++ src/ExpressValidator/ValidatorProfile.cs
@@ -0,0 +1,20 @@
+﻿namespace ExpressValidator
+{
+	public abstract class ValidatorProfile<T>
+	{
+		protected readonly ExpressValidatorBuilder<T> _validatorBuilder;
+
+		protected ValidatorProfile(OnFirstPropertyValidatorFailed onFirstPropertyValidatorFailed = OnFirstPropertyValidatorFailed.Continue)
+		{
+			_validatorBuilder = new ExpressValidatorBuilder<T>(onFirstPropertyValidatorFailed);
+		}
+
+		public IExpressValidator<T> CreateValidator()
+		{
+			Configure(_validatorBuilder);
+			return _validatorBuilder.Build();
+		}
+
+		public abstract void Configure(ExpressValidatorBuilder<T> expressValidatorBuilder);
+	}
+}
---
+++ tests/ExpressValidator.Tests/ValidatorProfileTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace ExpressValidator.Tests
+{
+	[TestFixture]
+	internal class ValidatorProfileTests
+	{
+		[Test]
+		public void Should_AllowDerivedClassesToConfigureValidator_WhenCreatingValidator()
+		{
+			// Arrange
+			var customProfile = new CustomValidatorProfile();
+
+			// Act
+			var validator = customProfile.CreateValidator();
+
+			// Assert
+			Assert.That(validator, Is.Not.Null);
+			Assert.That(customProfile.CustomConfigurationWasApplied, Is.True);
+		}
+	}
+
+	internal class CustomValidatorProfile : ValidatorProfile<string>
+	{
+		public bool CustomConfigurationWasApplied { get; private set; }
+
+		public CustomValidatorProfile(OnFirstPropertyValidatorFailed option = OnFirstPropertyValidatorFailed.Continue)
+			: base(option)
+		{
+		}
+
+		public override void Configure(ExpressValidatorBuilder<string> expressValidatorBuilder)
+		{
+			CustomConfigurationWasApplied = true;
+		}
+	}
+}

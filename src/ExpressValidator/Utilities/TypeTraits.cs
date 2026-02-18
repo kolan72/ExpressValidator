23c07f78e496db8925230c86c49c0aa99af0e390
kolan72 <kolan72@gmail.com>
2026-02-18 19:03:13 +0300

    Add `TypeName` property to `TypeTraits<T>`.

--- src/ExpressValidator/Utilities/TypeTraits.cs
+++ src/ExpressValidator/Utilities/TypeTraits.cs
@@ -5,5 +5,6 @@ namespace ExpressValidator
 	internal static class TypeTraits<T>
 	{
 		public static readonly bool CanBeNull = !typeof(T).IsValueType || Nullable.GetUnderlyingType(typeof(T)) != null;
+		public static readonly string TypeName = typeof(T).Name;
 	}
 }
